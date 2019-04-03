6085e384078f5a65b5d36b09502e04cb145eeb74
andrewlowell <andrewlowellr@gmail.com>
2019-04-03 17:03:34 -0600

    Finish MVP.

---
+++ auth/auth-router.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secret = require('../api/secrets').jwtSecret; // <<<<<<<<<<<<
+const Users = require('../users/users-model.js');
+
+// for endpoints beginning with /api/auth
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['student', 'ta'], // pretend they come from database user.roles
+  };
+  // removed the const secret from this line <<<<<<<<<<<<<<<<<<<<<<<
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secret, options); // returns valid token
+}
+
+module.exports = router;
